b0c68f3de2c45afe5d12d80e7837b5edc70e18c2
zhyiming67 <85863669+zhyiming67@users.noreply.github.com>
2021-06-24 16:41:59 -0700

    Add files via upload

---
+++ ToDoViews/Sources/ToDoViews/Models/ToDoItem.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Stehling, Brennan on 6/14/21.
+//
+import Amplify
+import Foundation
+
+/// Priority of Todo item.
+public enum ToDoPriority: String, CaseIterable, EnumPersistable {
+    case low
+    case medium
+    case high
+}
+
+/// Defination of Todo item. Contains id(unique, automatically generated), priority and text.
+public struct ToDoItem: Model, Identifiable, Hashable {
+    public let id: String
+    public var priority: ToDoPriority
+    public var text: String
+    public var completed: Bool
+    
+    public init(id: String? = nil, priority: ToDoPriority? = nil, text: String? = nil) {
+        self.id = id ?? UUID().uuidString
+        self.priority = priority ?? .medium
+        self.text = text ?? ""
+        self.completed = false
+    }
+}
+
+public typealias ToDoItems = [ToDoItem]
+
+extension ToDoItem: CustomStringConvertible {
+    public var description: String {
+        "\(text), \(priority) [\(id)]"
+    }
+}
---
+++ ToDoViews/Sources/ToDoViews/ToDoListRow.swift
@@ -0,0 +1,56 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Stehling, Brennan on 6/14/21.
+//
+
+import SwiftUI
+
+/// Display a row in the Todo list.
+public struct ToDoListRow: View {
+    ///Binding one TodoItem in the list.
+    @Binding var toDoItem: ToDoItem
+
+    public var body: some View {
+        HStack{
+            switch toDoItem.priority {
+            case .low:
+                VStack{
+                    Rectangle().frame(width: 8.0).foregroundColor(.blue)
+                }
+                Spacer().frame(width: 5)
+                HStack {
+                    Image(systemName: "l.circle").foregroundColor(.blue)
+                    Text(toDoItem.text).strikethrough(toDoItem.completed).foregroundColor(toDoItem.completed ? Color(.systemGray2) : .black)
+                }
+            case .medium:
+                VStack{
+                    Rectangle().frame(width: 8.0).foregroundColor(.yellow)
+                }
+                Spacer().frame(width: 5)
+                HStack {
+                    Image(systemName: "m.circle").foregroundColor(.yellow)
+                    Text(toDoItem.text).strikethrough(toDoItem.completed).foregroundColor(toDoItem.completed ? Color(.systemGray2) : .black)
+                }
+            case .high:
+                VStack{
+                    Rectangle().frame(width: 8.0).foregroundColor(.red)
+                }
+                Spacer().frame(width: 5)
+                HStack {
+                    Image(systemName: "h.circle").foregroundColor(.red)
+                    Text(toDoItem.text).strikethrough(toDoItem.completed).foregroundColor(toDoItem.completed ? Color(.systemGray2) : .black)
+                }
+            }
+        }
+        .animation(.spring(response: 2))
+    }
+}
+
+struct ToDoListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        let item = ToDoItem(text: "Eat breakfast")
+        ToDoListRow(toDoItem: .constant(item))
+    }
+}
